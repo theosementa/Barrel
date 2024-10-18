4b56778bbff52e279722da52f66306fd0793d949
Theo Sementa <theosementa@gmail.com>
2024-10-18 06:24:20 +0200

    Initial Commit

---
+++ Barrel/BarrelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarrelApp.swift
+//  Barrel
+//
+//  Created by Theo Sementa on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarrelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarrelTests/BarrelTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarrelTests.swift
+//  BarrelTests
+//
+//  Created by Theo Sementa on 18/10/2024.
+//
+
+import Testing
+@testable import Barrel
+
+struct BarrelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
